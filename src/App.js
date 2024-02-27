2549463c74f2dee14655a80bddcba4019a78ba06
nandan7198 <99603611+nandan7198@users.noreply.github.com>
2024-02-28 00:29:49 +0530

    Authentication

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import SignIn from "./Components/SignIn";
+import SignUp from "./Components/SignUp";
+import Products from "./Components/Products";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 function App() {
+  const [authenticated, setAuthenticated] = React.useState(false);
+  const navigate = useNavigate();
+  const handleLogin = (username, password) => {
+    // Perform authentication logic, e.g., check against hard-coded credentials
+    if (username === "admin" && password === "admin") {
+      setAuthenticated(true);
+    }
+  };
+  useEffect(() => {
+    if (authenticated) {
+      navigate("/home");
+    }
+  }, [authenticated, navigate]);
+
   return (
-    <div className="App">
-      
-    </div>
+    <Routes>
+      <Route path="/signin" element={<SignIn onLogin={handleLogin} />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route
+        path="/home"
+        element={authenticated ? <Products /> : <Navigate to="/signin" />}
+      />
+    </Routes>
   );
 }
 
---
+++ src/Components/Products.js
@@ -0,0 +1,9 @@
+
+
+function Products() {
+    return(
+        <h1>Hello</h1>
+    );
+}
+
+export default Products;
